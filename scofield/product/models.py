c2bcaf077b8da2c83591624e4d777a5c53d04087
greg newman <greg@20seven.org>
2008-09-30 06:34:10 -0400

    adding admin.py

--- scofield/product/models.py
+++ scofield/product/models.py
@@ -13,6 +13,10 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False, help_text='Product Name')
     slug = models.SlugField(max_length=210, null=False, blank=False, help_text='Used for URLs, auto-generated from name if blank')
     sku = models.CharField(max_length=100, null=True, blank=True)
-    category = models.ManyToManyField(Category, blank=True)
-    manufacturer = models.ForeignKey(Manufacturer, blank=False)         
+    category = models.ManyToManyField(Category, blank=False, null=False)
+    manufacturer = models.ForeignKey(Manufacturer, blank=True, null=True)
+    msrp = models.DecimalField(max_digits=14, decimal_places=2)
+
+    class Meta:
+        abstract = True
 
