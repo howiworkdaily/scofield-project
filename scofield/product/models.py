ae38884444be3b3e0f98ca406352fe92037423f1
greg newman <greg@20seven.org>
2008-09-30 06:52:07 -0400

    making the products model abstract

--- scofield/product/models.py
+++ scofield/product/models.py
@@ -1,13 +1,26 @@
 from django.db import models
+from datetime import datetime
 
 from scofield.category.models import *
 from scofield.manufacturer.models import Manufacturer
 
 
-class Product(models.Model):
+class ProductModel(models.Model):
 
     """
     Base class for products
+    """
+
+    #timestamps
+    date_added = models.DateTimeField(default=datetime.now)
+    date_updated = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        abstract = True
+
+class Product(ProductModel):
+    """
+    Product Model
     """
 
     name = models.CharField(max_length=200, null=False, blank=False, help_text='Product Name')
@@ -17,6 +30,13 @@ class Product(models.Model):
     manufacturer = models.ForeignKey(Manufacturer, blank=True, null=True)
     msrp = models.DecimalField(max_digits=14, decimal_places=2)
 
-    class Meta:
-        abstract = True
+
+class Price(models.Model):
+
+    """
+    Base class for product pricing
+    """
+
+    product = models.ForeignKey(Product)
+    price = models.DecimalField(max_digits=14, decimal_places=2)
 
