5b41a6b2276feb74b9456640b9b6bf5b7834f27f
greg newman <greg@20seven.org>
2008-10-06 06:17:31 -0400

    modifying the product base class so i can merge my fork later

--- product/models.py
+++ product/models.py
@@ -15,19 +15,6 @@ class ProductModel(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False, help_text='Product Name')
     slug = models.SlugField(max_length=210, null=False, blank=False, help_text='Used for URLs, auto-generated from name if blank')
     sku = models.CharField(max_length=100, null=True, blank=True, help_text='Product SKU or Part Number')
-
-    #timestamps
-    date_added = models.DateTimeField(default=datetime.now)
-    date_updated = models.DateTimeField(default=datetime.now)
-
-    class Meta:
-        abstract = True
-
-class Product(ProductModel):
-    """
-    Product Model extends the abstract ProductModel
-    """
-
     category = models.ManyToManyField(Category, blank=False, null=False)
     manufacturer = models.ForeignKey(Manufacturer, blank=True, null=True)
     msrp = models.DecimalField(max_digits=14, decimal_places=2)
@@ -37,6 +24,21 @@ class Product(ProductModel):
     is_featured = models.BooleanField('Is Featured Product', default=False)
     related_products = models.ManyToManyField('self', blank=True, null=True, related_name='related_products')
 
+    #timestamps
+    date_added = models.DateTimeField(default=datetime.now)
+    date_updated = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        abstract = True
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+class Product(ProductModel):
+    """
+    Product Model extends the abstract ProductModel
+    """
+
+    
 
 
 class Price(models.Model):
