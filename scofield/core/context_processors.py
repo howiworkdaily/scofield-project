0854d45150f51cb811b9e81e4f3b2ca971e337d5
greg newman <greg@20seven.org>
2008-10-12 16:03:20 -0400

    adding todo for middleware

--- scofield/core/context_processors.py
+++ scofield/core/context_processors.py
@@ -3,6 +3,7 @@ from category.models import *
 from manufacturer.models import *
 
 def dynamic_menus(request): 
+    # TODO: move these into templatetags to reduce the number of queries
     return {
         'manufacturers': Manufacturer.objects.filter(published=True),
         'categories': Category.objects.filter(published=True),
