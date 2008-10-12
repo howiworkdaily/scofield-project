1f2c0e50fed4c97e4cf1c3aaaed35a9e14102720
greg newman <greg@20seven.org>
2008-10-12 18:12:56 -0400

    leaving the old middleware but adding comments on the reason it's still there

--- scofield/core/context_processors.py
+++ scofield/core/context_processors.py
@@ -3,7 +3,13 @@ from category.models import *
 from manufacturer.models import *
 
 def dynamic_menus(request): 
-    # TODO: move these into templatetags to reduce the number of queries
+    """
+    If middleware is your thing and you don't mind the overhead of unneeded queries in views that don't
+    require these menus, uncomment the following and add to your MIDDLEWARE_CLASSES, otherwise use the
+    templatetags.
+    """
+
     return {
-        'manufacturers': Manufacturer.objects.filter(published=True),
+        # 'manufacturers': Manufacturer.objects.filter(published=True),
+        # 'categories': Category.objects.filter(published=True),
     }
