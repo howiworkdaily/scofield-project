dd3c8c7914481ea99295a1a3ad03035b3ed160dc
kevin fricovsky <kevin@howiworkdaily.com>
2008-10-14 15:12:00 -0400

    added CategoryImage class

--- scofield/category/admin.py
+++ scofield/category/admin.py
@@ -1,8 +1,32 @@
 from models import *
+from forms import FileUploadForm
 from django.contrib import admin
 
+class CategoryImageInline(admin.TabularInline):
+    model = CategoryImage
+    form = FileUploadForm
+
 class CategoryOptions(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
     list_display = ['name', 'slug', 'parent', 'sortorder', 'published']
 
-admin.site.register(Category, CategoryOptions)
+    inlines = [
+        CategoryImageInline,
+    ]
 
+    fieldsets = (
+    (None, {
+                'fields': ('name', 'slug', 'parent', 'sortorder', 'published',)
+            }),
+    ('Meta options', {
+               'classes': ('collapse',),
+               'fields': ('meta_keywords', 'meta_description', )
+           }),
+    )
+    
+class CategoryImageAdmin(admin.ModelAdmin):
+    model = CategoryImage
+    form = FileUploadForm
+        
+admin.site.register(CategoryImage, CategoryImageAdmin)
+admin.site.register(Category, CategoryOptions)
